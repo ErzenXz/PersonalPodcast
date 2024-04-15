04d2325f7be123cee969f9518d27bea62daf662b
ErzenXz <njnana2017@gmail.com>
2024-04-16 01:10:13 +0200

    Shtova nje User kontroller te pa funksionalizuar me vetem endpoints, si dhe te gjitha Modelet e nevojshme.

---
+++ back/PersonalPodcast/Models/Podcast.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonalPodcast.Models
+{
+    public class Podcast
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long Category { get; set; }
+        public string Tags { get; set; }
+        public string PosterImg { get; set; }
+        public string AudioFileUrl { get; set; }
+        public string VideoFileUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public long Views { get; set; }
+        public long PublisherId { get; set; }
+    }
+}
---
+++ back/PersonalPodcast/Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalPodcast.Models
+{
+    public class Rating
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public long EpisodeId { get; set; }
+        public int RatingValue { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
