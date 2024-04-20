244b504f2e7033b9211d7c2c6974f0f9f6cb2684
AuronIsmajli2001 <auronismajli2001@gmail.com>
2024-04-20 20:42:37 +0200

    qato controllera

---
+++ back/PersonalPodcast/DTO/PodcastRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalPodcast.DTO
+{
+    public class PodcastRequest
+    {
+        public long id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long CategoryId { get; set; }
+        public string Tags { get; set; }
+        public string PosterImg { get; set; }
+        public string AudioFileUrl { get; set; }
+        public string VideoFileUrl { get; set;}
+        public long PublisherId { get; set; }
+    }
+}
