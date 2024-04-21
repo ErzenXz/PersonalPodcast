ee69642ec63139edc4adcfb1cb02f96c1d60bf92
Erzen Krasniqi <njnana2017@gmail.com>
2024-04-21 03:51:27 +0200

    bug fix

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 // import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
@@ -42,12 +41,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <RouterProvider router={router} />
-
-import App from "./App.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
-      <App />
-
    </React.StrictMode>
 );
