ceb7c021cc8f9472479aab804eec7fb49f8725e6
arbreshea <105796280+arbreshea@users.noreply.github.com>
2024-04-27 23:25:17 +0200

    Create About.tsx

---
+++ front/src/pages/About.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import check_if_logged_in from "../services/is_logged";
+import Navigation from "../components/Navigation";
+import "../scss/About.scss";
+
+function About() {
+   // Change the document title
+   document.title = "About - Mergim Cahani";
+
+   const [loggedIn, setLoggedIn] = useState(false);
+
+   useEffect(() => {
+      check_if_logged_in().then((value) => {
+         setLoggedIn(value);
+      });
+   }, []);
+
+   return (
+      <>
+         {loggedIn == true ? (
+            <Navigation title="Mergim Canhasi" loggedIn={true} />
+         ) : (
+            <Navigation title="Mergim Canhasi" />
+         )}
+
+         <div className="about">
+            <h1>About</h1>
+            <p>
+               Hello there! My name is Mergim Cahani and I am a software developer. I have been
+               working with web technologies for over 10 years now. I have worked with a variety of
+               technologies, including HTML, CSS, JavaScript, PHP, and MySQL. I have also worked
+               with a variety of frameworks and libraries, including React, Angular, and Vue.js. I
+               am currently working as a full-stack developer at a software company in Prishtina,
+               Kosovo.
+            </p>
+         </div>
+      </>
+   );
+}
+
+export default About;
